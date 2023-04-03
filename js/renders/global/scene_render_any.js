2e4f19d81f75604c5f1697dffac28cbeacf8a8c6
joshm-sv <112880651+joshm-sv@users.noreply.github.com>
2023-04-03 21:19:37 +0800

    fixing Scene Undefined Lazy Load

--- js/renders/global/scene_render_any.js
+++ js/renders/global/scene_render_any.js
@@ -1,35 +1,26 @@
-$(document).on('knack-scene-render.any', function(event, scene) {
-    var measurementID ='G-3XYXWWXX44';
-    if (typeof gtag ==='function'){
-    gtag('js', new Date());
-    gtag('config', measurementID, {
-    // disable the default pageview hit
+const measurementID = 'G-3XYXWWXX44';
+
+async function initializeGoogleAnalytics() {
+  if (typeof gtag !== 'function') {
+    await new Promise(resolve => {
+      window.dataLayer = window.dataLayer || [];
+      window.gtag = function gtag() { dataLayer.push(arguments); }
+      LazyLoad.js([`https://www.googletagmanager.com/gtag/js?id=${measurementID}`], resolve);
+      $('body').append(`<script class="lazyload" src="https://www.googletagmanager.com/gtag/js?id=${measurementID}></script>`)
+    });
+  }
+
+  gtag('config', measurementID, {
     send_page_view: false
-    });
-    gtag('event', 'page_view', {
-    page_title: location.pathname + location.search + location.hash,
-    page_location: location.search,
-    page_path: location.pathname
-    });
-    } else {
-    window.dataLayer = window.dataLayer || [];
-    LazyLoad.js([`https://www.googletagmanager.com/gtag/js?id=${measurementID}`],function(){
-    window.gtag = window.gtag || function gtag() { dataLayer.push(arguments); }
-    gtag('js', new Date());
-    gtag('config', measurementID, {
-    // disable the default pageview hit
-    send_page_view: false
-    });
-    // Manually send page_view events
-    gtag('event', 'page_view', {
-    page_title: location.pathname + location.search + location.hash,
-    page_location: location.search,
-    page_path: location.pathname
-    });
-    });
-    }
-    $('.kn-modal-bg').off('click'); //Will make the modal popup uncloseable when the outside is clicked
-    // if( $('#kn-scene_1904').length === 0){
-    //     $('.sv_carousel_bs').remove()
-    // }
   });
+
+  gtag('event', 'page_view', {
+    page_title: location.pathname + location.search + location.hash,
+    page_location: location.search,
+    page_path: location.pathname
+  });
+}
+
+$(document).on('knack-scene-render.any', function(event, scene) {
+  initializeGoogleAnalytics();
+});
