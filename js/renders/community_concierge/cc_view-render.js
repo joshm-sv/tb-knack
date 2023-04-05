3b958da0381c790a310f5d0930d7f53103276325
joshm-sv <112880651+joshm-sv@users.noreply.github.com>
2023-04-05 20:23:57 +0800

    added custom function export

--- js/renders/community_concierge/cc_view-render.js
+++ js/renders/community_concierge/cc_view-render.js
@@ -1,3 +1,5 @@
+const { addStyleSheet } = require('/js/custom_functions/custom');
+
 //scene_1254
 $(document).on('knack-view-render.view_3754', function(event, view, data){
     // $('#kn-scene_1253').removeClass('group-layout-wrapper')
@@ -118,6 +120,253 @@ $(document).on('knack-form-submit.view_3568', function(event, view, record) {
           location.href = location.href + `popup-cm-activity-logs-in-devs/${Knack.views.view_3374.record.id}/`
     });
 });
+
+
+function appendCustomMenuMain() {
+    //Menu Icon
+    var dashboard = `<i class="fa fa-th-large"></i>`;
+    var serviceHistory = `<i class="fa fa-history "></i>`;
+    var infractions = `<i class="fa fa-info-circle"></i>`;
+    var units = `<i class="fa fa-tachometer"></i>`;
+    var hazards = `<i class="fa fa-life-ring"></i>`;
+    var supportTicket = `<i class="fa fa-inbox"></i>`;
+    var billing = `<i class="fa fa-list-alt"></i>`;
+    var profile = `<i class="fa fa-user"></i>`;
+    //
+    var radarLog = `<i class="fa fa-camera"></i>`;
+    var comLog = `<i class="fa fa-building-o"></i>`;
+    var userVidGuides = `<i class="fa fa-book"></i>`;
+ 
+ 
+    var customMenu = $(`
+        <ul id="custom_menu_id" style="list-style-type:none; display:none;">
+          <li id="com_dashboard"><a class="sv-cm-navbar" >${dashboard} <span class="custom-menu-text"> Dashboard</span></a></li>
+          <li id="com_service_history"><a class="sv-cm-navbar" >${serviceHistory} <span class="custom-menu-text"> Service History</span></a></li>
+          <li id="com_infractions"><a class="sv-cm-navbar" >${infractions} <span class="custom-menu-text"> Infractions</span></a></li>
+          
+          <li id="com_hazards"><a class="sv-cm-navbar" >${hazards}<span class="custom-menu-text"> Service Hazards</span></a></li>
+          <li id="com_support_tickets"><a class="sv-cm-navbar" >${supportTicket} <span class="custom-menu-text"> Support Tickets</span></a></li>
+      <li id="com_radar"><a class="sv-cm-navbar" >${units}</i> <span class="custom-menu-text"> Community Units</span></a></li>
+      <li id="com_radarLogs"><a class="sv-cm-navbar" >${radarLog} <span class="custom-menu-text"> RADAR Logs</span></a></li>
+          <li id="com_account_billing" style="display: none;"><a class="sv-cm-navbar" >${billing} <span class="custom-menu-text"> Account/Billing</span></a></li>
+          <li id="com_profile"><a class="sv-cm-navbar" >${profile} <span class="custom-menu-text"> Community Profile</span></a></li>
+          <li id="user_guide"><a class="sv-cm-navbar" >${userVidGuides} <span class="custom-menu-text"> Video Training</span></a></li>
+ 
+          
+          <li id="com_comLogs" style="display: none;"><a class="sv-cm-navbar" >${comLog} <span class="custom-menu-text"> Community Logs</span></a></li>
+      </ul>`
+        //HIDDEN
+        //Com log
+        //Account Billing
+    );
+ 
+    if ($('#custom_menu_id').length == 0) {
+        $('#kn-mobile-menu').append(customMenu);
+    }
+    $($('.kn-mobile-controls')[0]).on('click', function() {
+        showHideMenuText();
+    })
+ 
+    if ($('.kn-back-link').length == 1) {
+        let goBackTop = $('.kn-back-link').clone().attr("id", "sv-top-back").appendTo("#knack-dist_1");
+        let origLink = $($('.kn-back-link a')[0]).attr('href');
+ 
+        var timeout = setTimeout(function() {
+            let orig = $($('.kn-back-link a')[0]).attr('href');
+            $("#sv-top-back a").attr("href", origLink).addClass("sv-top-back");
+            clearTimeout(timeout);
+        }, 1000);
+    } else {
+        let origLink = $($('.kn-back-link a')[0]).attr('href');
+        let origText = $($('.kn-back-link a')[0])[0].innerHTML;
+ 
+        var timeout = setTimeout(function() {
+            let orig = $($('.kn-back-link a')[0]).attr('href');
+            $("#sv-top-back a").attr("href", origLink).addClass("sv-top-back");
+            $("#sv-top-back a").html(origText)
+            clearTimeout(timeout);
+        }, 200);
+    }
+}
+
+function showHideMenuText() {
+    if ($(".minimizedMenu")[0]) {
+        $('.custom-menu-text').addClass('hideMe');
+        $('#custom_menu_id li').addClass('bigger-icon');
+        $('.sv-bottom-menu').addClass('bigger-icon');
+        $('#custom_menu_id').addClass('adjust-margin-top-60px');
+    
+    } else {
+        $('.custom-menu-text').removeClass('hideMe');
+        $('#custom_menu_id li').removeClass('bigger-icon');
+        $('.sv-bottom-menu').removeClass('bigger-icon');
+        $('#custom_menu_id').removeClass('adjust-margin-top-60px');
+    }
+}
+
+//add href to the navbar
+function addLinkToCommunityDashboard(cid) {
+    appendCustomMenuBottom();
+    $('.field_1629').hide();
+ 
+    let varId = cid;
+    $('#com_dashboard a').attr("href", "https://apps.sv.knack.com/trashdash#community-concierge/community-dashboard/" + varId);
+    $('#com_service_history a').attr("href", "https://apps.sv.knack.com/trashdash#community-concierge/community-dashboard/" + varId + "/community-service-history/" + varId);
+    $('#com_infractions a').attr("href", "https://apps.sv.knack.com/trashdash#community-concierge/community-dashboard/" + varId + "/infractions/" + varId);
+    $('#com_radar a').attr("href", "https://apps.sv.knack.com/trashdash#community-concierge/community-dashboard/" + varId + "/radar-and-units/" + varId);
+    $('#com_hazards a').attr("href", "https://apps.sv.knack.com/trashdash#community-concierge/community-dashboard/" + varId + "/service-hazards/" + varId);
+    $('#com_support_tickets a').attr("href", "https://apps.sv.knack.com/trashdash#community-concierge/community-dashboard/" + varId + "/community-support-tickets/" + varId);
+    $('#com_account_billing a').attr("href", "https://apps.sv.knack.com/trashdash#community-concierge/community-dashboard/" + varId + "/account-billing/" + varId);
+    $('#com_profile a').attr("href", "https://apps.sv.knack.com/trashdash#community-concierge/community-dashboard/" + varId + "/my-community-profile/" + varId);
+    //
+    $('#com_radarLogs a').attr("href", "https://apps.sv.knack.com/trashdash#community-concierge/community-dashboard/" + varId + "/radar-logs/" + varId);
+    $('#com_comLogs a').attr("href", "https://apps.sv.knack.com/trashdash#community-concierge/community-dashboard/" + varId + "/community-logs/" + varId);
+    //back_to_dashboard
+    $('#sv-top-back a').attr("href", "https://apps.sv.knack.com/trashdash#community-concierge/community-dashboard/" + varId);
+    $('#user_guide a').attr("href", "https://apps.sv.knack.com/trashdash#community-concierge/community-dashboard/" + varId + "/cc-training-v2/" + varId);
+    var timeout = setTimeout(function() {
+        document.getElementById('custom_menu_id').style.display = 'block';
+        clearTimeout(timeout);
+    }, 1000);
+}
+
+function appendCustomMenuBottom() {
+    $($('.show-breadcrumbs')[0]).remove()
+    if ($('#community_concierge_theme').length == 0) {
+        $('head').append('<link rel="stylesheet" type="text/css" id="community_concierge_theme" class="lazyload" href="https://strategic-visionz.github.io/knack-css/scss/Community%20Manager%20Theme.min.css">')
+    }
+    let customMenu = $('#custom_menu_id');
+ 
+    var myComIcon = `<i class="fa fa-building"></i>`;
+    var logOutIcon = `<i class="fa fa-sign-out"></i>`;
+ 
+    $('#kn-mobile-menu').append(customMenu);
+    var customMenuBottom = $(`
+        <ul class="sv-bottom-menu" style="list-style-type: none;">
+          <li><a href="https://apps.sv.knack.com/trashdash#community-concierge">  ${myComIcon} <span class="custom-menu-text"> My Communities </span></a></li>
+          <li><a href="#" class="kn-log-out">${logOutIcon} <span class="custom-menu-text"> Log Out</a> </span></li></ul>
+      </ul>`);
+    var checkIfExist = document.getElementsByClassName('sv-bottom-menu');
+    if (checkIfExist.length == 0) {
+        $('#kn-mobile-menu').append(customMenuBottom);
+    }
+ 
+}
+
+
+ $(document).on("knack-view-render.view_3458", function(event, view, data) {
+    appendCustomMenuMain();
+    addLinkToCommunityDashboard(data.id);
+    $('#com_service_history').addClass('sv-active');
+ });
+ 
+ var fromEmailOn = false;
+ $(document).on("knack-view-render.view_4260", function(event, view, data) {
+    var comID = data.field_1500_raw[0].id
+    appendCustomMenuMain();
+    addLinkToCommunityDashboard( comID );
+    $('#com_hazards').addClass('sv-active');
+    if(!fromEmailOn && Knack.hash_parts[2] == 'addressed-service-hazards'){
+      window.location.href = `${Knack.url_base}#community-concierge/community-dashboard/${comID}/?hazard_id=${data.id}`;
+      fromEmailOn = true;
+    }
+ });
+ 
+ //Infractions view_3561
+ $(document).on("knack-view-render.view_3561", function(event, view, data) {
+    appendCustomMenuMain();
+    addLinkToCommunityDashboard(data.id);
+    $('#com_infractions').addClass('sv-active');
+ });
+ //Radar and Units view_3465
+ $(document).on("knack-view-render.view_3465", function(event, view, data) {
+    appendCustomMenuMain();
+    addLinkToCommunityDashboard(data.id);
+    $('#com_radar').addClass('sv-active');
+ 
+    if (data.field_1647 === 0) {
+        $('#view_3465 #field_1647 span').style.display = "none";
+    }
+ });
+ //view_3560 Service Hazards
+ $(document).on("knack-view-render.view_3560", function(event, view, data) {
+    appendCustomMenuMain();
+    addLinkToCommunityDashboard(data.id);
+    $('#com_hazards').addClass('sv-active');
+ });
+ 
+ //Service Hazards view_3463 UNSURE
+ $(document).on("knack-view-render.view_3463", function(event, view, data) {
+    appendCustomMenuMain();
+    addLinkToCommunityDashboard(data.id);
+ });
+ //Support Ticket view_3468
+ $(document).on("knack-view-render.view_3468", function(event, view, data) {
+    appendCustomMenuMain();
+    addLinkToCommunityDashboard(data.id);
+    $('#com_support_tickets').addClass('sv-active');
+ });
+ //Account Billing view_3470
+ $(document).on("knack-view-render.view_3470", function(event, view, data) {
+    appendCustomMenuMain();
+    addLinkToCommunityDashboard(data.id);
+    $('#com_account_billing').addClass('sv-active');
+ });
+ //Profile view_3472 ? UNSURE
+ $(document).on("knack-view-render.view_3472", function(event, view, data) {
+    appendCustomMenuMain();
+    addLinkToCommunityDashboard(data.id);
+    $('#com_profile').addClass('sv-active');
+ });
+ //view_3551 Community Profile
+ $(document).on("knack-view-render.view_3551", function(event, view, data) {
+    appendCustomMenuMain();
+    addLinkToCommunityDashboard(data.id);
+    $('#com_profile').addClass('sv-active');
+ });
+ //view_3477
+ $(document).on("knack-view-render.view_3477", function(event, view, data) {
+    appendCustomMenuMain();
+    addLinkToCommunityDashboard(data.id);
+ });
+ 
+ //view_3536 Edit Community Profile
+ $(document).on("knack-view-render.view_3536", function(event, view, data) {
+    appendCustomMenuMain();
+    addLinkToCommunityDashboard(data.id);
+ });
+ 
+ //view_3587 RADAR Logs
+ $(document).on("knack-view-render.view_3587", function(event, view, data) {
+    appendCustomMenuMain();
+    addLinkToCommunityDashboard(data.id);
+    $('#com_radarLogs').addClass('sv-active');
+ });
+ 
+ //view_3586 Community Logs
+ $(document).on("knack-view-render.view_3586", function(event, view, data) {
+    appendCustomMenuMain();
+    addLinkToCommunityDashboard(data.id);
+ });
+ 
+ //view_3604 Maange Access Code
+ $(document).on("knack-view-render.view_3604", function(event, view, data) {
+    appendCustomMenuMain();
+    addLinkToCommunityDashboard(data.id);
+ });
+ 
+ //Community Units - view_3512
+ $(document).on("knack-view-render.view_3512", function(event, view, data) {
+    appendCustomMenuMain();
+    addLinkToCommunityDashboard(data.id);
+ 
+ });
+ 
+ //Community Units - view_5247
+ $(document).on("knack-view-render.view_5247", function(event, view, data) {
+    appendCustomMenuMain();
+    addLinkToCommunityDashboard(data.id);
+ });
 
 //SIDE BAR
 //DASHBOARD
@@ -431,3 +680,111 @@ $(document).on('knack-view-render.view_5840', function(event, view, data){ //QUA
        }
      }
 });
+
+//Documents Btn Function helper
+function documentFunctions(data){
+    //docs_no_data
+    !data.field_1533 & !data.field_2904 & !data.field_1534 & !data.field_1535 ? $('#docs_no_data').show() : $('#docs_no_data').hide(); 
+  if (!data.field_1533) {
+        $('#doc_resident_flyer_btn').addClass('hideMe'); //Resident Flyer
+    }
+    if (!data.field_2904) {
+        $('#doc_com_flyer_btn').addClass('hideMe'); //Community Flyer
+        $('#doc_com_flyer_btn').addClass('sv-no-data');
+        $('#doc_com_flyer_btn_download').addClass('hideMe')
+    }
+    data.field_1534 ? $('#doc_cert_of_ins_btn').show() : $('#doc_cert_of_ins_btn').hide();
+    if (!data.field_1535) {
+        $('#doc_holiday_flyer_btn').hide(); //Holiday Flyer
+    }
+ 
+    $('#doc_resident_flyer_btn').on('click', function() { //Resident Flyer
+        $('.field_1533 a').click();
+    });
+    $('#doc_com_flyer_btn').on('click', function() { //Community Flyer
+        $('#cmFlyer').click();
+    });
+    $('#doc_com_flyer_btn_download').on('click', function() { //Community Flyer
+        document.getElementById('cmFlyer').click()
+    });
+    $('#doc_cert_of_ins_btn').on('click', function() { //Certificate of Ins
+        $('.field_1534 a').click();
+    });
+    $('#doc_holiday_flyer_btn').on('click', function() { //Holiday Flyer
+        $('.field_1535 a').click();
+    });
+}
+ 
+ //view_3713 Community Profile Documents BTN
+ $(document).on("knack-view-render.view_3713", function(event, view, data) {
+    $('#view_3713').addClass('hideMe');
+    documentFunctions(data);
+      var cmFlyerCloudField = data.field_2904_raw;
+     $('#cmFlyer').attr('href', cmFlyerCloudField )
+ });
+ 
+ $(document).on("knack-view-render.view_5243", function(event, view, data) {
+    $('#kn-scene_1908').removeClass('kn-scene').removeClass('kn-container').css('text-align', 'center')
+    $('#view_5243 .field_2556').hide()
+    documentFunctions(data);
+    var cmFlyerCloudField = data.field_2556_raw;
+    var customFlyerCloud =  `<a class="kn-button is-primary" id="cmFlyer" href="${cmFlyerCloudField}" target="_blank">Download Community Flyer</a>`
+    var iframe = `<iframe src="${cmFlyerCloudField}" width="100%" height="600" style="border:none;"></iframe>`
+    $('#view_5243').append(customFlyerCloud)
+ });
+ 
+ $(document).on("knack-view-render.view_2536", function(event, view, data) {
+    $('#view_2536 .field_2557 img').hide()
+    $('#view_2536 .field_2558 img').hide()
+    $('#view_2536 .field_2559 img').hide()
+    documentFunctions(data);
+    var cmFlyerCloudField = data.field_2557_raw;
+    var cmFlyerCloudField2 = data.field_2558_raw;
+    var cmFlyerCloudField3 = data.field_2559_raw;
+ 
+    var customFlyerCloud =  `<a class="kn-button is-primary" id="cmFlyer" href="${cmFlyerCloudField}" target="_blank">Download Contract Document</a>`
+    var customFlyerCloud2 =  `<a class="kn-button is-primary" id="cmFlyer2" href="${cmFlyerCloudField2}" target="_blank">Download Contract Addendum</a>`
+    var customFlyerCloud3 =  `<a class="kn-button is-primary" id="cmFlyer3" href="${cmFlyerCloudField3}" target="_blank">Download Master Agreement</a>`
+ 
+    $('#view_2536 .field_2557 .kn-detail-body').append(customFlyerCloud)
+    $('#view_2536 .field_2558 .kn-detail-body').append(customFlyerCloud2)
+    $('#view_2536 .field_2559 .kn-detail-body').append(customFlyerCloud3)
+ });
+ 
+ $(document).on("knack-view-render.view_1825", function(event, view, data) {
+    $('#view_1825 .field_2552 img').hide()
+    $('#view_1825 .field_2495 img').hide()
+    $('#view_1825 .field_2904 img').hide()
+    $('#view_1825 .field_2553 img').hide()
+    $('#view_1825 .field_2495 kn-detail-body span').hide()
+ 
+    documentFunctions(data);
+    var cmFlyerCloudField = data.field_2552_raw;
+    var cmFlyerCloudField2 = data.field_2904_raw;
+    var cmFlyerCloudField3 = data.field_2553_raw;
+ 
+ 
+    if (cmFlyerCloudField != ""){
+      var fileNameDotIdx = cmFlyerCloudField.lastIndexOf(".");
+      var fileName = cmFlyerCloudField.substr(0, fileNameDotIdx);
+ 
+      updatedImageUrl = fileName + ".jpg";
+ 
+      var cloudImage = `<span><a class="kn-img-gallery" href="#"><span class=""><img data-kn-img-gallery="${updatedImageUrl}" class="nailthumb-image" src="${updatedImageUrl}"></span></a></span>`
+      var customFlyerCloud =  `<a class="kn-button is-primary" id="cmFlyer" href="${cmFlyerCloudField}" target="_blank">Download Map</a>`
+      
+      $('#view_1825 .field_2552 .kn-detail-body').append(customFlyerCloud)
+      $('#view_1825 .field_2495 .kn-detail-body').append(cloudImage)
+    }
+ 
+    if (cmFlyerCloudField2 != ""){
+        var customFlyerCloud2 =  `<a class="kn-button is-primary" href="${cmFlyerCloudField2}" target="_blank">Download PDF Flyer</a>`
+        $('#view_1825 .field_2904 .kn-detail-body').append(customFlyerCloud2)
+    }
+ 
+    if (cmFlyerCloudField3 != ""){
+        var customFlyerCloud3 =  `<a class="kn-button is-primary" href="${cmFlyerCloudField3}" target="_blank">Download Letter</a>`
+        $('#view_1825 .field_2553 .kn-detail-body').append(customFlyerCloud3)
+    }
+ });
+ 
