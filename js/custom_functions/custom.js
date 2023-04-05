fe5331561ad496849f2d3d60abe70f53a580cebd
joshm-sv <112880651+joshm-sv@users.noreply.github.com>
2023-04-06 01:48:47 +0800

    Groupings - Organize custom.js

--- js/custom_functions/custom.js
+++ js/custom_functions/custom.js
@@ -1,34 +1,3 @@
-function addCCStyleSheet(){
-    if ($('#community_concierge_theme').length == 0) {
-          $('head').append('<link rel="stylesheet" type="text/css" id="community_concierge_theme" class="lazyload" href="https://strategic-visionz.github.io/knack-css/scss/Community%20Manager%20Theme.min.css">')
-          $('head').append(`<script>
-      (function(h,o,t,j,a,r){
-          h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-          h._hjSettings={hjid:2773425,hjsv:6};
-          a=o.getElementsByTagName('head')[0];
-          r=o.createElement('script');r.async=1;
-          r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-          a.appendChild(r);
-      })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-   </script>`)
-      }
-}
-
-function showHideMenuText() {
-    if ($(".minimizedMenu")[0]) {
-        $('.custom-menu-text').addClass('hideMe');
-        $('#custom_menu_id li').addClass('bigger-icon');
-        $('.sv-bottom-menu').addClass('bigger-icon');
-        $('#custom_menu_id').addClass('adjust-margin-top-60px');
-
-    } else {
-        $('.custom-menu-text').removeClass('hideMe');
-        $('#custom_menu_id li').removeClass('bigger-icon');
-        $('.sv-bottom-menu').removeClass('bigger-icon');
-        $('#custom_menu_id').removeClass('adjust-margin-top-60px');
-    }
-}
-
 
 var currentEstDT = new Date();
 var currentDate = currentEstDT.toLocaleDateString('en-US', { month: '2-digit', day: '2-digit', year: 'numeric', timeZone: 'America/New_York' });
@@ -57,9 +26,7 @@ function prevServiceDate(){
  }
 
 module.exports = {
-    addCCStyleSheet: addCCStyleSheet,
     searchParam: searchParam,
-    showHideMenuText: showHideMenuText,
     prevServiceDate: prevServiceDate,
     currentServiceDate: currentServiceDate
 };
--- js/renders/community_concierge/cc_view-render.js
+++ js/renders/community_concierge/cc_view-render.js
@@ -1,9 +1,40 @@
-const { addStyleSheet, searchParam } = require('/js/custom_functions/custom');
+const { searchParam } = require('/js/custom_functions/custom');
 var asHeader = {
     'X-Knack-Application-Id': Knack.application_id,
     "X-Knack-REST-API-Key": window.knackRestApiKey,
     'Authorization': Knack.getUserToken()
 };
+
+function addStyleSheet(){
+    if ($('#community_concierge_theme').length == 0) {
+          $('head').append('<link rel="stylesheet" type="text/css" id="community_concierge_theme" class="lazyload" href="https://strategic-visionz.github.io/knack-css/scss/Community%20Manager%20Theme.min.css">')
+          $('head').append(`<script>
+      (function(h,o,t,j,a,r){
+          h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+          h._hjSettings={hjid:2773425,hjsv:6};
+          a=o.getElementsByTagName('head')[0];
+          r=o.createElement('script');r.async=1;
+          r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+          a.appendChild(r);
+      })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+   </script>`)
+      }
+}
+
+function showHideMenuText() {
+    if ($(".minimizedMenu")[0]) {
+        $('.custom-menu-text').addClass('hideMe');
+        $('#custom_menu_id li').addClass('bigger-icon');
+        $('.sv-bottom-menu').addClass('bigger-icon');
+        $('#custom_menu_id').addClass('adjust-margin-top-60px');
+
+    } else {
+        $('.custom-menu-text').removeClass('hideMe');
+        $('#custom_menu_id li').removeClass('bigger-icon');
+        $('.sv-bottom-menu').removeClass('bigger-icon');
+        $('#custom_menu_id').removeClass('adjust-margin-top-60px');
+    }
+}
 
 //scene_1254
 $(document).on('knack-view-render.view_3754', function(event, view, data){
